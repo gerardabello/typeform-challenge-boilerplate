3e11ad5fdbbcdcf6e2c29071699b75e75944d94c
Gerard Abelló Serras <gerardabello@gmail.com>
2017-07-19 12:09:01 +0200

    Display title and background

--- src/app.js
+++ src/app.js
@@ -9,19 +9,20 @@ const Root = styled.section`
   width: 100vw;
   height: 100vh;
   overflow: scroll;
+  background: ${p => p.background};
 `
 
 export default class App extends Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = { fetching: true }
   }
 
   async fetchForm () {
     try {
       let form = await getForm('vDGGs9')
 
-      this.setState({ form })
+      this.setState({ form, fetching: false })
     } catch (reason) {
       console.warn(reason)
     }
@@ -32,17 +33,23 @@ export default class App extends Component {
   }
 
   render () {
+    if (this.state.fetching) {
+      return <p> Fetching form... </p>
+    }
+
+    const form = this.state.form
+
     return (
-      <Root>
+      <Root background={form.theme.colors.background}>
         <WelcomeScreen
-          title='shop foo'
+          title={form.title}
           description='description foo'
           img='https://goo.gl/images/o8kxDT'
         />
-        <Page color='red' />
-        <Page color='green' />
-        <Page color='red' />
-        <Page color='green' />
+        <Page />
+        <Page />
+        <Page />
+        <Page />
       </Root>
     )
   }
--- src/page.js
+++ src/page.js
@@ -1,16 +1,17 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 
 const Fitter = styled.div`
   position: relative;
   width: 100vw;
   height: 100vh;
+  box-shadow: inset 0 0 0 1px black;
 `
 
 class Page extends Component {
   render () {
     return (
-      <Fitter style={{background: this.props.color}}>
+      <Fitter>
         {this.props.children}
       </Fitter>
     )
