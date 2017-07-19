f45b156ac121bf2e5764e3a47fe4c554e326e8a2
Gerard Abelló Serras <gerardabello@gmail.com>
2017-07-19 23:30:01 +0200

    Style product

--- src/product.js
+++ src/product.js
@@ -17,19 +17,27 @@ const Picture = styled.div`
   align-items: center;
   img {
     border-radius: 5px;
-    box-shadow: 0px 20px 20px rgba(0, 0, 0, 0.28);
+    box-shadow: 0px 30px 40px -20px rgba(0, 0, 0, 0.28);
     width: 70vw;
   }
 `
 
 const Description = styled.div`
+  padding-top: 20px;
   width: 100%;
   text-align: center;
+  font-size: 22px;
+  color: ${p => p.theme.colors.answer};
 `
 
 const Price = styled.div`
+  padding-top: 10px;
+  padding-bottom: 20px;
+  font-size: 18px;
+  font-weight: 600;
   width: 100%;
   text-align: center;
+  color: ${p => p.theme.colors.question};
 `
 
 const Button = styled.button`
@@ -39,7 +47,7 @@ const Button = styled.button`
   border: none;
   outline: none;
   font-size: 14px;
-  padding: 12px 20px;
+  padding: 12px 30px;
   color: ${p => p.theme.colors.buttonText};
   background: ${p => p.theme.colors.button};
 `
