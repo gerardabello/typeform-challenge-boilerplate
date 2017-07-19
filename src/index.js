1586181403e316784ae5e7fcbf5e7f3c3b0a2d52
Gerard Abelló Serras <gerardabello@gmail.com>
2017-07-19 18:53:24 +0200

    Render app

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './app'
-import ShoppingCart from './shopping-cart'
 
 require('babel-polyfill')
 
 export function mount ({ rootNode }) {
-  ReactDOM.render(<ShoppingCart />, rootNode)
+  ReactDOM.render(<App />, rootNode)
 }
 
 mount({
