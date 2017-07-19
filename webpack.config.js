5bf5fbc00e0cce847ac6754f79fe6f4f4b41b461
Gerard Abelló Serras <gerardabello@gmail.com>
2017-07-19 12:04:07 +0200

    New feature: console.log

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,10 @@
 const path = require('path')
-const webpack = require('webpack')
 
 module.exports = {
   devtool: 'source-map',
   entry: {
     bundle: './src/index.js'
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: true,
-
-      compress: {
-        warnings: false,
-        sequences: true,
-        dead_code: true,
-        conditionals: true,
-        booleans: true,
-        unused: true,
-        if_return: true,
-        join_vars: true,
-        drop_console: true
-      },
-
-      sourceMap: true
-    })
-  ],
   output: {
     library: ['bundle'],
     libraryTarget: 'var',
