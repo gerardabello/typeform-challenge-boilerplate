427014c9ef365780596a25935e098b6061ecf38c
Gerard Abelló Serras <gerardabello@gmail.com>
2017-07-20 13:43:51 +0200

    Style shopping card page

--- src/shopping-cart.js
+++ src/shopping-cart.js
@@ -3,7 +3,19 @@ import styled from 'styled-components'
 import R from 'ramda'
 
 const Cart = styled.div`
-
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: center;
+  height: 100%;
+`
+const Title = styled.h2`
+  font-size: 20px;
+  color: ${p => p.theme.colors.question};
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    padding-bottom: 20px;
+      margin-bottom: 12px;
 `
 const Total = styled.div``
 
@@ -12,6 +24,8 @@ const Item = styled.div`
   justify-content: space-between;
   align-items: center;
   margin: 10px 0;
+      padding-bottom: 20px;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 `
 
 const Image = styled.img`
@@ -33,14 +47,21 @@ const Name = styled.div`
 const Button = styled.button`
   outline: none;
   border: none;
-  background: ${p => p.theme.colors.button}
-  color: ${p => p.theme.colors.buttonText}
+  width: 100%;
+  font-size: 14px;
+  padding: 14px;
+  border-radius: 3px;
+  font-weight: 500;
+  background: ${p => p.theme.colors.button};
+  color: ${p => p.theme.colors.buttonText};
+  margin-top: 12px;
 `
 
 const ShoppingCart = ({ cart, price, onNext }) => {
   const uniqCart = R.groupWith(R.equals, cart)
   return (
     <Cart>
+      <Title>Shopping Cart</Title>
       {uniqCart.map((order, i) => {
         const { name, price, image } = R.nth(0, order)
         return (
