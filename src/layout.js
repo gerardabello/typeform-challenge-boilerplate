88762fb346968abf4d49ca109284c14e1c92ba0a
carlesba <carlesba.mail@gmail.com>
2017-07-20 16:53:28 +0200

    put down arrow in all rows

--- src/layout.js
+++ src/layout.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Swiper from 'react-swipeable'
 import R from 'ramda'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 import Gallery from './gallery'
 import Statement from './statement'
@@ -20,6 +20,35 @@ const GalleryWrapper = styled.div`
     )
   );
   transition: 300ms cubic-bezier(0.25, 0.46, 0.45, 0.94) all;
+`
+
+const bounceAnimation = keyframes`
+  0%, 20%, 50%, 80%, 100% {
+    -moz-transform: translateY(0);
+    -ms-transform: translateY(0);
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+  40% {
+    -moz-transform: translateY(-30px);
+    -ms-transform: translateY(-30px);
+    -webkit-transform: translateY(-30px);
+    transform: translateY(-30px);
+  }
+  60% {
+    -moz-transform: translateY(-15px);
+    -ms-transform: translateY(-15px);
+    -webkit-transform: translateY(-15px);
+    transform: translateY(-15px);
+  }
+`
+
+const Arrow = styled.div`
+  ${(p) => p.hide ? 'display: none;' : ''}
+  position: fixed;
+  bottom: 10px;
+  left: calc(50% - 20px);
+  animation: ${bounceAnimation} 3s infinite;
 `
 
 class Layout extends Component {
@@ -147,6 +176,14 @@ class Layout extends Component {
       fieldIndex: index
     })
   }
+  shouldHideArrow () {
+    const {horitzontalDelta, verticalDelta, fieldIndex} = this.state
+    return (
+      this.props.fields.length - 1 === fieldIndex ||
+      horitzontalDelta !== 0 ||
+      verticalDelta !== 0
+    )
+  }
 
   render () {
     const { fields } = this.props
@@ -216,6 +253,13 @@ class Layout extends Component {
             }
           })}
         </GalleryWrapper>
+        <Arrow hide={this.shouldHideArrow()}>
+          <img
+            width='40'
+            height='40'
+            src='data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjwhRE9DVFlQRSBzdmcgIFBVQkxJQyAnLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4nICAnaHR0cDovL3d3dy53My5vcmcvR3JhcGhpY3MvU1ZHLzEuMS9EVEQvc3ZnMTEuZHRkJz48c3ZnIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDMyIDMyIiBoZWlnaHQ9IjMycHgiIGlkPSLQodC70L7QuV8xIiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCAzMiAzMiIgd2lkdGg9IjMycHgiIHhtbDpzcGFjZT0icHJlc2VydmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiPjxwYXRoIGQ9Ik0yNC4yODUsMTEuMjg0TDE2LDE5LjU3MWwtOC4yODUtOC4yODhjLTAuMzk1LTAuMzk1LTEuMDM0LTAuMzk1LTEuNDI5LDAgIGMtMC4zOTQsMC4zOTUtMC4zOTQsMS4wMzUsMCwxLjQzbDguOTk5LDkuMDAybDAsMGwwLDBjMC4zOTQsMC4zOTUsMS4wMzQsMC4zOTUsMS40MjgsMGw4Ljk5OS05LjAwMiAgYzAuMzk0LTAuMzk1LDAuMzk0LTEuMDM2LDAtMS40MzFDMjUuMzE5LDEwLjg4OSwyNC42NzksMTAuODg5LDI0LjI4NSwxMS4yODR6IiBmaWxsPSIjMTIxMzEzIiBpZD0iRXhwYW5kX01vcmUiLz48Zy8+PGcvPjxnLz48Zy8+PGcvPjxnLz48L3N2Zz4='
+          />
+        </Arrow>
       </Swiper>
     )
   }
--- src/welcome-screen.js
+++ src/welcome-screen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 
 const Fitter = styled.div`
   position: relative;
@@ -24,46 +24,11 @@ const Description = styled.div`
   text-align: center;
 `
 
-const bounceAnimation = keyframes`
-  0%, 20%, 50%, 80%, 100% {
-    -moz-transform: translateY(0);
-    -ms-transform: translateY(0);
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-  }
-  40% {
-    -moz-transform: translateY(-30px);
-    -ms-transform: translateY(-30px);
-    -webkit-transform: translateY(-30px);
-    transform: translateY(-30px);
-  }
-  60% {
-    -moz-transform: translateY(-15px);
-    -ms-transform: translateY(-15px);
-    -webkit-transform: translateY(-15px);
-    transform: translateY(-15px);
-  }
-`
-
-const Arrow = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: calc(50% - 20px);
-  animation: ${bounceAnimation} 3s infinite;
-`
-
 const WelcomeScreen = ({ title, img, description }) => {
   return (
     <Fitter>
       <Picture src={img} />
       <Description dangerouslySetInnerHTML={{ __html: description }} />
-      <Arrow>
-        <img
-          width='40'
-          height='40'
-          src='data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjwhRE9DVFlQRSBzdmcgIFBVQkxJQyAnLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4nICAnaHR0cDovL3d3dy53My5vcmcvR3JhcGhpY3MvU1ZHLzEuMS9EVEQvc3ZnMTEuZHRkJz48c3ZnIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDMyIDMyIiBoZWlnaHQ9IjMycHgiIGlkPSLQodC70L7QuV8xIiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCAzMiAzMiIgd2lkdGg9IjMycHgiIHhtbDpzcGFjZT0icHJlc2VydmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiPjxwYXRoIGQ9Ik0yNC4yODUsMTEuMjg0TDE2LDE5LjU3MWwtOC4yODUtOC4yODhjLTAuMzk1LTAuMzk1LTEuMDM0LTAuMzk1LTEuNDI5LDAgIGMtMC4zOTQsMC4zOTUtMC4zOTQsMS4wMzUsMCwxLjQzbDguOTk5LDkuMDAybDAsMGwwLDBjMC4zOTQsMC4zOTUsMS4wMzQsMC4zOTUsMS40MjgsMGw4Ljk5OS05LjAwMiAgYzAuMzk0LTAuMzk1LDAuMzk0LTEuMDM2LDAtMS40MzFDMjUuMzE5LDEwLjg4OSwyNC42NzksMTAuODg5LDI0LjI4NSwxMS4yODR6IiBmaWxsPSIjMTIxMzEzIiBpZD0iRXhwYW5kX01vcmUiLz48Zy8+PGcvPjxnLz48Zy8+PGcvPjxnLz48L3N2Zz4='
-        />
-      </Arrow>
     </Fitter>
   )
 }
